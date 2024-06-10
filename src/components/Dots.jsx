6b12c45bbcb5681d48e9d5731db62b71bd881dbf
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-06-10 21:28:39 +0530

    Fixed dynamic colour allocation issue

--- src/components/Dots.jsx
+++ src/components/Dots.jsx
@@ -1,12 +1,23 @@
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 
 export default function Dots({ color }) {
-  return (
-    <div className={`h-6 w-6 rounded-full aspect-square bg-${color}-500`}>
-    </div>
-  );
+    const colorClass = {
+        red: 'bg-red-500',
+        green: 'bg-green-500',
+        amber: 'bg-amber-500',
+    };
+    const colorHoverClass = {
+        red: 'hover:bg-red-600',
+        green: 'hover:bg-green-600',
+        amber: 'hover:bg-amber-600',
+    }
+    return (
+        <div
+            className={`h-6 w-6 rounded-full aspect-square cursor-pointer ${colorClass[color]} ${colorHoverClass[color]}`}
+        ></div>
+    );
 }
 
 Dots.propTypes = {
-  color: PropTypes.string
-}
+    color: PropTypes.string,
+};
