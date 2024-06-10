7938db48c91deae03f883807c86f67ab1ab7d543
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-06-11 01:04:09 +0530

    Browser Body Component Customization

--- src/components/BrowserBody.jsx
+++ src/components/BrowserBody.jsx
@@ -1,8 +1,8 @@
-
 export default function BrowserBody() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div className="flex h-full items-center gap-10 justify-center p-6">
+            <div className="h-full w-1/2 border-4 border-black rounded-xl flex items-center justify-center p-5 flex-col"></div>
+            <div className="h-full w-1/2 border-4 border-black rounded-xl flex items-center justify-center p-5 flex-col"></div>
+        </div>
+    );
 }
